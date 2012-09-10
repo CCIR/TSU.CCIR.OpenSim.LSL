181466f1d28a56eebab70c8fde08026dfd8e7895
SignpostMarv <github@signpostmarv.name>
2012-09-10 13:51:20 +0100

    adding an LSL Util class with methods useful region modules in converting an LSL list to a typed list without having duplicated conversion code

---
+++ TeessideUniversity.CCIR.OpenSim/LSLUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+using OpenSim.Region.ScriptEngine.Shared;
+
+namespace TeessideUniversity.CCIR.OpenSim
+{
+    public static class LSLUtil
+    {
+        public static List<T> TypedList<T>(LSL_Types.list list, T defaultValue)
+        {
+            return TypedList<T>(list.Data, defaultValue);
+        }
+
+        public static List<T> TypedList<T>(object[] list, T defaultValue)
+        {
+            List<T> resp = new List<T>(list.Length);
+            foreach(object o in list)
+            {
+                try
+                {
+                    resp.Add((T)TypeDescriptor.GetConverter(
+                            typeof(T)).ConvertFrom(o));
+                }
+                catch
+                {
+                    resp.Add(defaultValue);
+                }
+            }
+
+            return resp;
+        }
+    }
+}
