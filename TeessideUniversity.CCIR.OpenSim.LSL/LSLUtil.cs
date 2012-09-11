daed764bf7e2b0cbc3aea24be4e0d8fd8856ec6b
SignpostMarv <github@signpostmarv.name>
2012-09-11 12:07:31 +0100

    adding method for getting a list of attach points

--- TeessideUniversity.CCIR.OpenSim.LSL/LSLUtil.cs
+++ TeessideUniversity.CCIR.OpenSim.LSL/LSLUtil.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 
 using OpenSim.Region.ScriptEngine.Shared;
+using OpenSim.Region.ScriptEngine.Shared.ScriptBase;
 
 namespace TeessideUniversity.CCIR.OpenSim
 {
@@ -27,6 +28,67 @@ namespace TeessideUniversity.CCIR.OpenSim
                     resp.Add(defaultValue);
                 }
             }
+
+            return resp;
+        }
+
+        public static List<int> AttachmentList(LSL_Types.list list)
+        {
+            return AttachPoints(list.Data);
+        }
+
+        public static List<int> AttachPoints(object[] list)
+        {
+            List<int> resp = new List<int>();
+            foreach (object o in list)
+            {
+                try
+                {
+                    resp.Add((int)TypeDescriptor.GetConverter(
+                            typeof(int)).ConvertFrom(o));
+                }
+                catch { }
+            }
+
+            resp.RemoveAll(point =>
+            {
+                switch (point)
+                {
+                    case ScriptBaseClass.ATTACH_CHEST:
+                    case ScriptBaseClass.ATTACH_HEAD:
+                    case ScriptBaseClass.ATTACH_LSHOULDER:
+                    case ScriptBaseClass.ATTACH_RSHOULDER:
+                    case ScriptBaseClass.ATTACH_LHAND:
+                    case ScriptBaseClass.ATTACH_RHAND:
+                    case ScriptBaseClass.ATTACH_LFOOT:
+                    case ScriptBaseClass.ATTACH_RFOOT:
+                    case ScriptBaseClass.ATTACH_BACK:
+                    case ScriptBaseClass.ATTACH_PELVIS:
+                    case ScriptBaseClass.ATTACH_MOUTH:
+                    case ScriptBaseClass.ATTACH_CHIN:
+                    case ScriptBaseClass.ATTACH_LEAR:
+                    case ScriptBaseClass.ATTACH_REAR:
+                    case ScriptBaseClass.ATTACH_LEYE:
+                    case ScriptBaseClass.ATTACH_REYE:
+                    case ScriptBaseClass.ATTACH_NOSE:
+                    case ScriptBaseClass.ATTACH_RUARM:
+                    case ScriptBaseClass.ATTACH_RLARM:
+                    case ScriptBaseClass.ATTACH_LUARM:
+                    case ScriptBaseClass.ATTACH_LLARM:
+                    case ScriptBaseClass.ATTACH_RHIP:
+                    case ScriptBaseClass.ATTACH_RULEG:
+                    case ScriptBaseClass.ATTACH_RLLEG:
+                    case ScriptBaseClass.ATTACH_LHIP:
+                    case ScriptBaseClass.ATTACH_LULEG:
+                    case ScriptBaseClass.ATTACH_LLLEG:
+                    case ScriptBaseClass.ATTACH_BELLY:
+                    case ScriptBaseClass.ATTACH_LEFT_PEC:
+                    case ScriptBaseClass.ATTACH_RIGHT_PEC:
+                        return false;
+                    default:
+                        return true;
+                }
+            });
 
             return resp;
         }
