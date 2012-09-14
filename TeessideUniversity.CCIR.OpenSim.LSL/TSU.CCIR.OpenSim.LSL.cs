38d913c5aa6c34a9a3b958166b143bf20aa039a3
SignpostMarv <github@signpostmarv.name>
2012-09-14 13:35:23 +0100

    no longer using OriginPartID

--- TeessideUniversity.CCIR.OpenSim.LSL/TSU.CCIR.OpenSim.LSL.cs
+++ TeessideUniversity.CCIR.OpenSim.LSL/TSU.CCIR.OpenSim.LSL.cs
@@ -132,7 +132,6 @@ namespace TeessideUniversity.CCIR.OpenSim
             else
             {
                 newGroup.FromPartID = host;
-                newGroup.OriginPartID = host;
                 newGroup.ScheduleGroupForFullUpdate();
                 IScriptModule[] modules = m_scene.RequestModuleInterfaces<IScriptModule>();
                 foreach (IScriptModule module in modules)
